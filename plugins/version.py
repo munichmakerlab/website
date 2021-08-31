f6971b923d38e01bffef97670dfa5677e228ff0e
Severin Schols <severin@schols.de>
2021-08-31 23:10:31 +0200

    update details on how to find the space, incl. the map

--- plugins/version.py
+++ plugins/version.py
@@ -15,7 +15,7 @@ def templateTags():
 
 # def preBuild(site):
 # 	print 'preBuild'
-# 
+#
 # def postBuild(site):
 # 	print 'postBuild'
 
@@ -24,7 +24,7 @@ def templateTags():
 # def preBuildPage(site, path, context, data):
 # 	print 'preBuildPage', path
 # 	return context, data
-# 
+#
 # def postBuildPage(site, path):
 # 	print 'postBuildPage', path
 # 	pass
@@ -33,26 +33,26 @@ def templateTags():
 # Deploy actions
 
 def preDeploy(site):
-	
+
 	# Add a deploy log at /versions.txt
-	
+
 	import urllib2
 	import datetime
 	import platform
 	import codecs
 	import getpass
-	
+
 	url = site.config.get('aws-bucket-website')
 	data = u''
-	
+
 	# If this is the first deploy we don't have to fetch the old file
 	if url:
 		try:
 			data = urllib2.urlopen('http://%s/versions.txt' % url, timeout=8.0).read() + u'\n'
 		except:
-			print "Could not fetch the previous versions.txt, skipping..."
+			print("Could not fetch the previous versions.txt, skipping...")
 			return
-	
+
 	data += u'\t'.join([datetime.datetime.now().isoformat(), platform.node(), getpass.getuser()])
 	codecs.open(os.path.join(site.paths['build'], 'versions.txt'), 'w', 'utf8').write(data)
 
