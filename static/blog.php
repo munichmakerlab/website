59cef9baa16027572ea092b35e22ffff8092de5e
Severin Schols <severin@schols.de>
2022-07-30 22:57:34 +0200

    update from APC to APCu to fix the blg on our website

--- static/blog.php
+++ static/blog.php
@@ -1,12 +1,12 @@
 <?php
 header('Content-Type: text\javascript');
 $json_https = "";
-if (apc_exists("mumalab_json")) {
-	$json_https = apc_fetch("mumalab_json");
+if (apcu_exists("mumalab_json")) {
+	$json_https = apcu_fetch("mumalab_json");
 } else {
 	$json = file_get_contents('http://log.munichmakerlab.de/api/read/json');
 	$json_https = preg_replace("#http:\\\/\\\/(\d+)\.media\.tumblr\.com#", "https://$1.media.tumblr.com",$json);
-	apc_store("mumalab_json", $json_https, 300);
+	apcu_store("mumalab_json", $json_https, 300);
 }
 echo $json_https;
 ?>
