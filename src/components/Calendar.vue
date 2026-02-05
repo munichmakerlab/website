2d56bf46ce268551fdd97e1a1cead136448dac1f
Aaron Soehnen <git.aaron@soehnen.net>
2026-02-05 19:01:20 +0100

    tune calendar display

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -8,13 +8,33 @@
 import { ref } from 'vue'
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 import iCalendarPlugin from '@fullcalendar/icalendar'
+import listPlugin from '@fullcalendar/list'
 
 const calendarOptions = ref({
-  plugins: [dayGridPlugin, iCalendarPlugin],
+  plugins: [dayGridPlugin, iCalendarPlugin, listPlugin, timeGridPlugin],
   initialView: 'dayGridMonth',
+  firstDay: 1,
+  locale: 'de',
+  eventTimeFormat: {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  },
+  slotLabelFormat: {
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false
+  },
+  eventDidMount: (info) => {
+    // This adds a native 'hover' tooltip using the HTML title attribute
+    if (info.event.title) {
+      info.el.setAttribute('title', info.event.title);
+    }
+  },
   eventSources: [
-{
+    {
       url: '/api/get-calendar?source=pretix',
       format: 'ics',
       color: '#3788d8',
@@ -30,7 +50,7 @@ const calendarOptions = ref({
   headerToolbar: {
     left: 'prev,next today',
     center: 'title',
-    right: 'dayGridMonth,dayGridWeek'
+    right: 'dayGridMonth,timeGridWeek,listWeek'
   }
 })
 </script>
