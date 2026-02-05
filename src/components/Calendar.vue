73a2678e50f1fd567ff3e356f70edce99d85278a
Aaron Soehnen <git.aaron@soehnen.net>
2026-02-05 16:44:56 +0100

    add fullcalendar for vue

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,43 +1,32 @@
 <template>
-  <div v-if="!hasConsent" class="consent">
-    We use Google Calendar to manage our Events. If you want to see them you
-    have to consent to send data to Google.
-    <button @click="consent">Consent</button>
-  </div>
-  <div v-if="hasConsent" class="calendar">
-    <iframe
-      src="https://calendar.google.com/calendar/embed?src=lbd0aa2rlahecp7juvp35hd0k0%40group.calendar.google.com&ctz=Europe%2FBerlin"
-      style="border: 0"
-      width="800"
-      height="600"
-      frameborder="0"
-      scrolling="no"
-    ></iframe>
+  <div class="calendar-container">
+    <FullCalendar :options="calendarOptions" />
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { ref } from 'vue'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import iCalendarPlugin from '@fullcalendar/icalendar'
 
-const hasConsent = computed(() => {
-  return window.localStorage.getItem('calendarConsent')
+const calendarOptions = ref({
+  plugins: [dayGridPlugin, iCalendarPlugin],
+  initialView: 'dayGridMonth',
+  eventSources: [
+    {
+      url: 'https://tickets.munichmakerlab.de/mumalab/events/ical/?locale=de',
+      format: 'ics'
+    },
+    {
+      url: 'https://calendar.google.com/calendar/ical/lbd0aa2rluhecp7juvp35hd0k0%40group.calendar.google.com/public/basic.ics',
+      format: 'ics'
+    }
+  ],
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,dayGridWeek'
+  }
 })
-const consent = () => {
-  localStorage.setItem('calendarConsent', true)
-  window.location.reload()
-}
 </script>
-
-
-<style>
-button {
-    display: block;
-    margin: auto;
-    background: none;
-    border-color: var(--text-color);
-    font-size: 1.5rem;
-    border-radius: 0.5rem;
-    cursor: pointer;
-    margin-top: 2rem;
-}
-</style>
